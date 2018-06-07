61fc88e4e5bc612204b80af29461a10429cf8147
manishchopra@myself.com <manishchopra@myself.com>
2018-06-07 10:55:09 +0100

    Add project files.

---
+++ AngularJSRouting/Scripts/Script.js
@@ -0,0 +1,34 @@
+﻿/// <reference path="angular.min.js" />
+
+
+
+var app = angular.module("myModule", ["ngRoute"]);
+        app.config(function ($routeProvider, $locationProvider) {
+            $routeProvider
+                .when("/london", {
+                    templateUrl: "Templates/London.html",
+                    controller: "londonController"
+                })
+                .when("/paris", {
+                    templateUrl: "Templates/Paris.html",
+                    controller: "parisController"
+                })
+                .when("/tokyo", {
+                    templateUrl: "Templates/Tokyo.html",
+                    controller: "tokyoController"
+                })
+                .otherwise({
+                    redirectTo: "/london"
+                });
+
+            $locationProvider.html5Mode(true);
+        })
+        .controller("londonController", function($scope){
+            //add in whatever controller specific code here
+        })
+        .controller("parisController", function ($scope) {
+            //add in whatever controller specific code here
+        })
+        .controller("tokyoController", function ($scope) {
+            //add in whatever controller specific code here
+        });
